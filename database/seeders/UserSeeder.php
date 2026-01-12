0dd732b7aafb60d6a2bd56b375eb57abfd8be75f
adityaprayogaw <adityaprayoga1263@gmail.com>
2026-01-12 11:51:18 +0800

    update userSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Membuat akun Tenant 
+        User::create([
+            'name' => 'Made Tenant',
+            'email' => 'tenant@gmail.com',
+            'password' => Hash::make('password123'),
+            'role' => 'tenant',
+            'phone_whatsapp' => '08123456789',
+        ]);
+
+        // Membuat akun Owner
+        User::create([
+            'name' => 'Krisna Owner',
+            'email' => 'owner@gmail.com',
+            'password' => Hash::make('password123'),
+            'role' => 'owner',
+            'phone_whatsapp' => '08987654321',
+        ]);
+    }
+}
