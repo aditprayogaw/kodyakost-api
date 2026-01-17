8799174775b9315905b8fa6f8486c6056933f39a
adityaprayogaw <adityaprayoga1263@gmail.com>
2026-01-17 18:06:23 +0800

    tes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -39,10 +39,7 @@ return new class extends Migration
             $table->integer('last_activity')->index();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('users');
